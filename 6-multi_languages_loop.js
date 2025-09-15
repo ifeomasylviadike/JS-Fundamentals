fa3198b2596ba81cef14e5a505e187177f88ef05
ifeomasylviadike <ifeomasylvia.dike@gmail.com>
2025-09-15 04:10:28 +0100

    edit

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -4,6 +4,18 @@ let thirdLine = "JavaScript is amazing"
 
 const sentences = [firstLine, secondLine, thirdLine];
 
-for(let i=0; i<sentences.length; i++){
+for (let i = 0; i < sentences.length; i++){
     console.log(sentences[i]);
 }
+
+// const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// for (let i = 0; i < languages.length; i++) {
+//   console.log(languages[i]);
+// }
+
+// const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+// languages.forEach(function(words){
+//     console.log(words)
+// ;
+// })
