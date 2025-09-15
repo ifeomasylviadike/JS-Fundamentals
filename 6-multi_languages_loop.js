daeccfc94f7932670d1140d5d70cb9509e968477
ifeomasylviadike <ifeomasylvia.dike@gmail.com>
2025-09-15 03:57:23 +0100

    multilanguages loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+let firstLine = "C is fun";
+let secondLine = "Python is cool";
+let thirdLine = "JavaScript is amazing"
+
+const sentences = [firstLine, secondLine, thirdLine];
+
+for(let i=0; i<sentences.length; i++){
+    console.log(sentences[i]);
+}
